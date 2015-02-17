22b329230ecc3372f7af4e32f0709a2502b928b1
Drew Raines <drew@raines.me>
2015-02-16 21:03:17 -0600

    HW1: Don't need the 2 clause

--- src/Cis194/Hw1.hs
+++ src/Cis194/Hw1.hs
@@ -35,5 +35,4 @@ type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi 1 a b _ = [(a, b)]
-hanoi 2 a b tmp = concat [[(a, tmp)], (hanoi 1 a b tmp), [(tmp, b)]]
 hanoi n a b c = concat [(hanoi (n - 1) a c b), (hanoi 1 a b c), (hanoi (n - 1) c b a)]
