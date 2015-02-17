e9fdf3f1e4469fa5b5bc404d08ce330ab48ad01b
Drew Raines <drew@raines.me>
2015-02-16 18:26:58 -0600

    HW1: Hanoi WIP

--- src/Cis194/Hw1.hs
+++ src/Cis194/Hw1.hs
@@ -26,3 +26,14 @@ doubleEveryOther = reverse . doubleFromLeft . reverse
 
 sumDigits :: [Integer] -> Integer
 sumDigits = sum . concat . map toDigits
+
+-----------
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi 2 a b tmp = concat [[(a, tmp)], (hanoi 1 a b tmp), [(tmp, b)]]
+hanoi n a b c = concat [(hanoi (n - 1) a b c), (hanoi 1 a b c), (hanoi (n - 1) c b a)]
