ee8716bb0c5d27f663d1e0f54e488a0944518876
Drew Raines <drew@raines.me>
2015-02-17 21:39:53 -0600

    HW2: Parse log lines

--- src/Cis194/Hw2.hs
+++ src/Cis194/Hw2.hs
@@ -3,13 +3,50 @@
 module Cis194.Hw2 where
 
 import Log (LogMessage(..), MessageType(..), testParse)
-import qualified Text.ParserCombinators.Parsec as Parsec
+import Text.ParserCombinators.Parsec
+import Control.Applicative hiding ((<|>))
+
+digits :: Parser Int
+digits = read' <$> many1 digit
+  where read' = read :: String -> Int
+
+warn :: Parser LogMessage
+warn = do
+  _ <- char 'W'
+  _ <- spaces
+  t <- digits
+  _ <- spaces
+  msg <- many1 anyChar
+  return $ LogMessage Warning t msg
+
+info :: Parser LogMessage
+info = do
+  _ <- char 'I'
+  _ <- spaces
+  t <- digits
+  _ <- spaces
+  msg <- many1 anyChar
+  return $ LogMessage Info t msg
+
+err :: Parser LogMessage
+err = do
+  _ <- char 'E'
+  _ <- spaces
+  code <- digits
+  _ <- spaces
+  t <- digits
+  _ <- spaces
+  msg <- many1 anyChar
+  return $ LogMessage (Error code) t msg
 
 parseMessage :: String -> LogMessage
 parseMessage m =
-  case Parsec.parse (Parsec.oneOf "IEW") "" m of
-    Right 'I' -> LogMessage Info 0 "foo"
-    Right 'W' -> LogMessage Warning 0 "foo"
-    Right 'E' -> LogMessage (Error 1) 0 "foo"
-    Right x -> Unknown ("what is " ++ show x)
-    Left err -> Unknown (show err)
+  case Text.ParserCombinators.Parsec.parse
+       (    err
+        <|> info
+        <|> warn) "" m of
+    Right l -> l
+    Left _ -> Unknown m
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
